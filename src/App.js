08dc9063b3ef62bbfb9b79e7f419a739fd4997c6
Parasbuda <parasbuda1234@gmail.com>
2024-01-09 10:29:24 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [size, setSize] = useState("");
+  const [stack, setStack] = useState([]);
+  const [limit, setLimit] = useState(0);
+
+  const generateStack = () => {
+    const destructureSize = size.split("*");
+    const length = destructureSize[0];
+    const height = destructureSize[1];
+    const width = destructureSize[2];
+    let array = [];
+    for (let i = 0; i < width; i++) {
+      let array1 = [];
+      for (let j = 0; j < length; j++) {
+        let array2 = [];
+        for (let k = 0; k < height; k++) {
+          array2.push({ position: `${i}${j}${k}`, occupied: false });
+        }
+        array1.push(array2);
+      }
+      array.push(array1);
+    }
+    setStack([...stack, array]);
+    setLimit(length * height * width);
+  };
+  console.log(stack,"as")
   return (
     <div className="App">
-      <h1>This is Starter Template of React App.</h1>
+      <h1>Location Mapping</h1>
+      <label htmlFor="size">Dimension of the Stack</label>
+      <br />
+      <input
+        type="text"
+        value={size}
+        style={{ width: "13%", marginTop: "5px", marginBottom: "5px" }}
+        onChange={(e) => setSize(e.target.value)}
+        placeholder="Enter the Length,Height,Width,(L*H*W)"
+      />
+      <br />
+      <button type="button" onClick={generateStack}>
+        Save
+      </button>
+
+      <br />
+
+      {/* displaying the stack */}
     </div>
   );
 }
